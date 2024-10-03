c5e52cae4053740605993a3a97bf854154704a47
Camille RIQUET <camille.riquet@Digital-Grenoble.local>
2024-10-03 16:40:28 +0200

    ajout start et create personnage

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        System.out.println(menu.start());
+        menu.start();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -7,7 +7,18 @@ public class Menu {
         scanner = new Scanner(System.in);
     }
 
-    public Personnage start(){
+    public void start() {
+        System.out.println("Taper votre choix: (Quit ou Creer)");
+        String choice = scanner.nextLine();
+        if (choice.equals("Quit")) {
+            System.exit(0);
+        }
+        if (choice.equals("Creer")) {
+            System.out.println(createPersonnage());
+        }
+    }
+
+    public Personnage createPersonnage() {
         System.out.println("Ecrire votre nom:");
         String name = scanner.nextLine();
         System.out.println("Ecrire votre type: Guerrier ou Magicien");
